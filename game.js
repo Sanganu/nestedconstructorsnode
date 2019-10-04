96815bcabf39d3fe5baea24283c2420a0ea58e29
Sanganu <kppkmsanu@gmail.com>
2019-10-04 09:13:34 -0600

    trying game setup

--- game.js
+++ game.js
@@ -5,15 +5,17 @@ var Gamecard = require("./gamecard.js");
 
 function Game(quiz) {
     this.quiz = [];
-    var allquestion = [];
+    this.allquestion = [];
+
     for (let i = 0; i < quiz.length; i++) {
         const gamecrd = new Gamecard(quiz[i].question, quiz[i].answer);
         allquestion.push(gamecrd);
     }
     console.log("Allquestion",allquestion);
-    this.quiz = quiz;
+    // this.quiz = quiz;
     this.score = 0;
-
+    this.currentquestion = 0;
+    
     this.checkScore = () => {
 
         for (let i = 0; i < allquestion.length; i++) {
@@ -25,21 +27,8 @@ function Game(quiz) {
     }
      
      this.startQuiz = () => {
-        // for(let i=0;i<allquestion.length;i++){
-
-
-          
-        //     allquestion[i].displayQuestion();
-        // }
         let i =0;
-        while(i<allquestion.length){
-            console.log("i",i," :",allquestion[i].question);
-            let reply = allquestion[i].displayQuestion();
-            if (reply){
-                i = i+1
-            }
-
-        }
+      
     }
     
 };
